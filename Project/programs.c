376c6e38ac2962c359fb43d117fa0f55c8ebdc56
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-10-25 17:57:55 +0545

    Update programs.c
    added a new function i.e. a game to guess a two digit number

--- Project/programs.c
+++ Project/programs.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<conio.h>
+#include<time.h>
+#include<stdlib.h>
+
+char *morse_codes;
 
 void home();
 int find_length_of(char *s); //returns length of a string
 void palindrome(); //asks for a string and says if it is palindrome or not
+void guess_number(); //guess a two digit number
 
 int main(){
 	home();
@@ -12,11 +17,13 @@ int main(){
 }
 
 void home(){
+	srand(time(NULL)); //seeding the random number generator
 	int n = 0;
 	printf("[ Welcome To All_In_1_C_Programs ]\n");
 	printf("----------------------------------\n");
 	//Options are still incomplete
 	printf("1. Palindrome Checker\n");
+	printf("2. Guess the number\n");
 	printf("----------------------------------\n");
 	printf("Choose one: ");
 	scanf("%d", &n);
@@ -25,6 +32,13 @@ void home(){
 		case 1:
 			//call palindrome function
 			palindrome();
+			break;
+		case 2:
+			//call palindrome function
+			guess_number();
+			break;
+		default:
+			printf("Invalid choice\n");
 			break;
 	}
 }
@@ -51,4 +65,46 @@ void palindrome(){
 		j--;
 	}
 	printf("Palindrome!\n");
+}
+
+void guess_number(){
+	int random_number = (rand() % 90) + 10;
+	int rleft = random_number, rright = random_number % 10, ileft, iright;
+	rleft = (rleft / 10) % 10;
+	int n = 0, tries = 0;
+	do{
+		printf("Enter the two digit number: ");
+		scanf("%d", &n);
+		if(n < 0){
+			printf("Reveal: %d\n", random_number);
+			printf("Enter the two digit number: ");
+			scanf("%d", &n);
+		}
+		tries++;
+		iright = n % 10;
+		ileft = n;
+		ileft = (ileft / 10 ) % 10;
+		
+		if(ileft != rleft && iright == rright){
+			printf("-+\n");
+		}
+		else if(ileft == rleft && iright != rright){
+			printf("+-\n");
+		}
+		else if(n == random_number){
+			printf("++\nYou Win in %d tries!\n", tries);
+		}
+		else if(ileft != rleft && ileft == rright && iright != rright && iright == rleft){
+			printf("##\n");
+		}
+		else if(iright != rright && iright == rleft && ileft != rleft){
+			printf("-#\n");
+		}
+		else if(ileft != rleft && ileft == rright && iright != rright){
+			printf("#-\n");
+		}
+		else if(ileft != rleft && iright != rright){
+			printf("--\n");
+		}
+	}while(n!=random_number);
 }
